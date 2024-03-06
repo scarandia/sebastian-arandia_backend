f5c895b585356fa8fca4bae83c4b84eda0cb8011
unknown <sc.arandia@gmail.com>
2024-03-06 12:11:21 -0400

    small changes

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -2,9 +2,6 @@ const express = require("express");
 const bodyParser = require('body-parser');
 const { processLoan, processPayment } = require('./models/processPay.js');
 const clientsRouter = require('./routes/clients');
-//const loansRouter = require('./routes/loans');
-//const paymentsRouter = require('./routes/payments');
-//const { validatePayment, LoanValidator } = require("./middleware/validateRequest");
 const expressValidator = require("express-validator");
 const Joi = require("joi");
 
@@ -27,7 +24,7 @@ const paymentSchema = Joi.object({
 /* If cases for schema validators */
 
 app.post('/loans', processLoan, (req, res) => {
-    const { error, value } = paymentSchema.validate(req.body)
+    const { error, value } = loanSchema.validate(req.body)
 
     if (error) {
         console.log(error);
@@ -53,10 +50,3 @@ app.use('/clients', clientsRouter);
 app.use(bodyParser.json());
 
 app.listen(PORT, () => console.log("Server Started on port " + PORT));
-
-/*
-app.post('/loans', LoanValidator, (req, res) => {
-    res.status(200).json({ message: 'Loan processed' });
-});
-*/
-//app.post('/payments', processPayment);
